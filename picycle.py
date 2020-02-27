16eef47737a25f33f29cd33325f275985d32f9ea
Troy Squillaci <troysquillaci@gmail.com>
2020-02-27 04:59:43 +0000

    Added Better GPS Capture & SQLite DB
    Signed-off-by: Troy Squillaci <troysquillaci@gmail.com>

--- picycle.py
+++ picycle.py
@@ -1,10 +1,75 @@
 import click
 import gpsd
 import gpxpy
+import sqlite3
+import sys
+import time
 
 from sense_hat import SenseHat
 from tabulate import tabulate
 
+SQLITE_CREATE_TABLE = """
+CREATE TABLE IF NOT EXISTS picycle (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  latitude FLOAT,
+  longitude FLOAT,
+  altitude FLOAT
+);
+"""
+
+SENSE_HAT_ERROR = [255, 0, 0]
+
+# -----------------------------------------------------------------------------
+# Database-related functions.
+# -----------------------------------------------------------------------------
+def create_connection(path):
+
+    connection = None
+
+    try:
+
+        connection = sqlite3.connect(path)
+        click.echo("Connection to SQLite database successful.")
+
+    except sqlite3.Error as e:
+
+        click.echo(f"Connection to SQLite database failed: '{e}'")
+
+    return connection
+
+def execute_query(connection, query):
+
+    cursor = connection.cursor()
+
+    try:
+
+        cursor.execute(query)
+        connection.commit()
+        click.echo("Query execution successful.")
+
+    except sqlite3.Error as e:
+
+        click.echo(f"Query execution failed: '{e}'")
+
+def execute_read_query(connection, query):
+
+    cursor = connection.cursor()
+    result = None
+
+    try:
+
+        cursor.execute(query)
+        result = cursor.fetchall()
+
+    except sqlite3.Error as e:
+
+        click.echo(f"Read query execution failed: '{e}'")
+
+    return result
+
+# -----------------------------------------------------------------------------
+# GPX-related functions.
+# -----------------------------------------------------------------------------
 def load_gpx_file(gpx_file):
 
     with open(gpx_file, "r") as f:
@@ -17,6 +82,9 @@ def save_gpx_file(gpx, gpx_file):
 
         f.write(gpx.to_xml())
 
+# -----------------------------------------------------------------------------
+# Picycle commands.
+# -----------------------------------------------------------------------------
 @click.group()
 def cli():
 
@@ -105,14 +173,32 @@ def info_routes(gpx_file):
     click.echo(tabulate(table, headers=["Latitude", "Longitude", "Elevation"], tablefmt="presto"))
 
 @cli.command()
-def gps():
+@click.option("--show/--no-show", default=False)
+def database(show):
 
-    gpsd.connect()
-    packet = gpsd.get_current()
-    print(packet.position())
+    # Connect to the SQLite database.
+    connection = create_connection("picycle.sqlite")
+
+    # If the connection is successful, then enter the control loop.
+    if connection:
+
+        # Show the contents of the table.
+        if show:
+
+            select_picycle = "SELECT * from picycle"
+            picycle = execute_read_query(connection, select_picycle)
+
+            for packet in picycle:
+
+                print(packet)
+
+    # Otherwise report the error and exit.
+    else:
+
+        sys.exit(1)
 
 @cli.command()
-def _():
+def record():
 
     # This will end up being the primary entry-point for the program when on
     # the bike. There are many actions this function should fulfill:
@@ -145,6 +231,55 @@ def _():
     # great software engineering skills to catch exceptions and recover are a
     # must.
 
+    # Initialize the Sense HAT.
     sense = SenseHat()
 
-    sense.show_message("Picycle")
+    # Connect to the SQLite database.
+    connection = create_connection("picycle.sqlite")
+
+    # Connect to the GPS device.
+    gpsd.connect()
+
+    # If the connection is successful, then enter the control loop.
+    if connection:
+
+        sense.show_message("Picycle")
+
+        # Create the database table.
+        execute_query(connection, SQLITE_CREATE_TABLE)
+
+        # Gather GPS data and save to the database.
+        while True:
+
+            packet = gpsd.get_current()
+            click.echo(f"GPS device connected to {packet.sats} satellites, mode {packet.mode}.")
+
+            try:
+
+                latitude, longitude = packet.position()
+                altitude = packet.altitude()
+
+                click.echo(f"{latitude}, {longitude}, {altitude}")
+
+                insert_packet = f"""
+                INSERT INTO
+                  picycle (latitude, longitude, altitude)
+                VALUES
+                  ({latitude}, {longitude}, {altitude});
+                """
+
+                execute_query(connection, insert_packet)
+
+            except gpsd.NoFixError as e:
+
+                click.echo(f"GPS device needs at least a 2D fix to provide position information.")
+
+            time.sleep(1)
+
+    # Otherwise report the error and exit.
+    else:
+
+        sense.show_letter("E", SENSE_HAT_ERROR)
+        time.sleep(3)
+        sense.clear()
+        sys.exit(1)
