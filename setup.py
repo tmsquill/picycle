5a9c76eacedec12af57142310ee27e777d0df32d
Troy Squillaci <troysquillaci@gmail.com>
2020-04-14 14:08:26 -0700

    Added Setup Scripts for PyPI
    Preparing source for PyPI, uploading with an entrypoint.
    
    Signed-off-by: Troy Squillaci <troysquillaci@gmail.com>

--- setup.py
+++ setup.py
@@ -3,11 +3,19 @@ from setuptools import setup
 setup(
     name="picycle",
     version="0.0.1",
+    license="MIT",
+    description="Put your Raspberry Pi to good use on a bike ride!",
+    author="Troy Squillaci",
+    author_email="troysquillaci@gmail.com",
+    url="https://github.com/tmsquill/picycle",
+    keywords=["raspberry pi", "sense hat", "gps", "sqlite", "bike"]
     py_modules=["picycle"],
     install_requires=[
         "Click",
         "colorama",
         "gpxpy",
+        "gpsd-py3",
+        "sense_hat",
         "tabulate"
     ],
     entry_points="""
